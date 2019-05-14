c644f0c9c155f981c03da4ed93a38206ae837d00
HolkerDev <nick.penkovskiy@gmail.com>
2019-05-14 12:43:09 +0200

    Startup, Entity configuration, API configuration, Functionality (add, edit, delete user data)

---
+++ GameBreak.Core/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using GameBreak.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBreak.Core
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options) { }
+
+        protected ApplicationDBContext()
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }
+    }
+}
